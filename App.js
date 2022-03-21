1de538e7fae8161d694de85e247633a56a3e0e45
hihami <jzh04082579@gmail.com>
2022-03-21 10:31:53 +0900

    3.2 TextInput

--- App.js
+++ App.js
@@ -1,39 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import react from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from 'react-native';
+import react,{useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { theme } from './colors';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState();
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
       <StatusBar style="auto"/>
       <View style={styles.header}>
-        <TouchableOpacity > 
-          {/* 터치하면 투명해짐, activeOpacity={0} 얘로 조절 ㄱㄴ */}
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color:working?"white":theme.grey}}>Work</Text>
         </TouchableOpacity>
-        {/* 터치ㅏ면 배경색 깜빡, activeOpacity={0.5}얘로 조절 0이랑 가까워지면 안보임 */}
-        {/* <TouchableHighlight 
-          underlayColor="red"
-          activeOpacity={0.5}
-          onPress={() => console.log("pressed")}>
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight> */}
-        {/* 작동은 되는데 ui에서는 티를 안냄 -> 콘솔에 로그는 찍힘 */}
-        {/* <TouchableWithoutFeedback
-          underlayColor="red"
-          activeOpacity={0.5}
-          onPress={() => console.log("pressed")}>
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableWithoutFeedback> */}
-        {/* 아무것도 안나타남, 여어가지 설정많음!!, new!! */}
-        {/* <Pressable>
-          <Text style={styles.btnText}>Travel</Text>
-        </Pressable> */}
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color:!working?"white":theme.grey}}>Travel</Text>
         </TouchableOpacity>
       </View>
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={working?"Add a To Do":"Where do you want to go?"} 
+        style={styles.input}/>
     </View>
   );
 }
@@ -52,6 +43,13 @@ const styles = StyleSheet.create({
   btnText:{
     fontSize: 38,
     fontWeight: "600",
-    color: "white",
+  },
+  input:{
+    backgroundColor:"white",
+    paddingVertical:10,
+    paddingHorizontal:20,
+    borderRadius:30, //동그랗게
+    marginTop:20,
+    fontSize:18,
   },
 });
